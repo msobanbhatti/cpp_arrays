d0829d040076349fce81c155c1b7523dd67a145d
msobanbhatti <msobanbhatti6@gmail.com>
2025-02-02 12:35:42 +0500

    practice arrays

---
+++ 1d_array.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int array[10]={35,4,8,508,9,5,100,14,58,95};
+    int max1,max2,max3;
+    max1=array[0];
+    max2=array[0];
+    max3=array[0];
+    for(int i=1;i<10;i++)
+    {
+        if(max1<array[i])
+        {
+            max3=max2;
+            max2=max1;
+            max1=array[i];
+        }
+        else if(max2<array[i])
+        {
+            max3=max2;
+            max2=array[i];
+        }
+        else if(max3<array[i])
+        {
+            max3=array[i];
+        }
+    }
+    cout<<"Maximum Number in array is "<<max1<<endl;
+    cout<<"2nd highest Number in array is "<<max2<<endl;
+    cout<<"3rd highest Number in array is "<<max3<<endl;
+    
+    
+    return 0;
+}
